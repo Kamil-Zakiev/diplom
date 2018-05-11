c0c35dfc58c3c123d925717112d16a11211aa286
Камиль Закиев <k.zakiev@bars-open.ru>
2018-05-11 16:45:34 +0300

    генерация сервиса для сборка исходного кода

---
+++ CsFileContentGrabbes/CsFileContentGrabbes/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace CsFileContentGrabbes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary> Сервис для сбора исходного кода дипломной работы </summary>
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rootDirectory = @"E:\Stash\diplom\EC_Console";
+            var csFileNames = Directory.GetFiles(rootDirectory, "*.cs", SearchOption.AllDirectories)
+                .Where(fileName => !fileName.Contains(@"\obj\") && !fileName.Contains(@"\bin\") &&
+                                   !fileName.Contains(@"AssemblyInfo"))
+                .Select(fileName => new
+                {
+                    FileName = fileName,
+                    MainPart = fileName.Substring(27)
+                })
+                .Select(dto =>
+                {
+                    var words = dto.MainPart.Split('\\');
+
+                    return new
+                    {
+                        ProjectName = words[0],
+                        RelativeFileName = string.Join("\\", words.Skip(1)),
+                        dto.FileName
+                    };
+                });
+
+            var groups = csFileNames.GroupBy(x => x.ProjectName);
+
+            var sourceCodeFile = @"E:\Stash\diplom\CsFileContentGrabbes\CsFileContentGrabbes\SourceCode.txt";
+            
+            var lines = new LinkedList<string>();
+            lines.AddFirst("Код актуален на дату: " + DateTime.Now.ToShortDateString());
+            lines.AddLast(string.Empty);
+            void WriteLine(string str)
+            {
+                lines.AddLast(str);
+            }
+            
+            foreach (var group in groups)
+            {
+                WriteLine(group.Key);
+                var projFiles = group.ToList();
+
+                var relativeFileNames = projFiles.Select(x => x.RelativeFileName);
+                foreach (var projFile in projFiles)
+                {
+                    WriteLine(projFile.RelativeFileName);
+                    foreach (var line in File.ReadAllLines(projFile.FileName))
+                    {
+                        WriteLine(line);
+                    }
+                    
+                    WriteLine(string.Empty);
+                }
+
+                WriteLine(string.Empty);
+                WriteLine(string.Empty);
+            }
+            
+            File.WriteAllLines(sourceCodeFile, lines);
+        }
+    }
+}
